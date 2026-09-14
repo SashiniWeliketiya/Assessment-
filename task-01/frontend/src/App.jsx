668b45ed26ef580aad64cb53b2c0073407ce3593
Sashini Ruwanthika Weliketiya <sashiniruwanthika08@gmail.com>
2026-09-14 18:15:26 +0530

    Add product detail route to App.jsx

--- task-01/frontend/src/App.jsx
+++ task-01/frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Storefront from './pages/Storefront'; 
 import Checkout from './pages/Checkout';
 import OrderHistory from './pages/OrderHistory';
+import ProductDetail from './pages/ProductDetail'; // 1. මෙතනින් import කරන්න
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Storefront />} />
+        <Route path="/product/:id" element={<ProductDetail />} /> {/* 2. මෙන්න මේ line එක අලුතින් දාන්න */}
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/orders" element={<OrderHistory />} />
       </Routes>

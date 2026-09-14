b98770e0818781dc35fe6384e998524343c0d3f6
Sashini Ruwanthika Weliketiya <sashiniruwanthika08@gmail.com>
2026-09-15 03:09:21 +0530

    Update API baseURL to live backend link

--- task-01/frontend/src/api/axiosInstance.js
+++ task-01/frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://ecommerce-backend-3h2jzepfi-sashini.vercel.app/',
+  baseURL: 'http://localhost:5000/api',
 });
 
 export default API;

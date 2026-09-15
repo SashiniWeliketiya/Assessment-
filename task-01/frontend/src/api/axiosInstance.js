5d0350adbf7c19d4727a304a72c5f24eb54b87bd
Sashini Ruwanthika Weliketiya <sashiniruwanthika08@gmail.com>
2026-09-15 16:48:28 +0530

    Add /api to backend baseURL

--- task-01/frontend/src/api/axiosInstance.js
+++ task-01/frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://ecommerce-backend-nu-ecru.vercel.app/',
+  baseURL: 'https://ecommerce-backend-nu.vercel.app/api',
 });
 
 export default API;

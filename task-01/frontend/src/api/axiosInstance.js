49c831c4a7202cdd522e7d1db4360be76b1059fe
Sashini Ruwanthika Weliketiya <sashiniruwanthika08@gmail.com>
2026-09-15 19:12:52 +0530

    Add /api to backend baseURL

--- task-01/frontend/src/api/axiosInstance.js
+++ task-01/frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://ecommerce-backend-qd2t38l66-sashini.vercel.app/api',
+  baseURL: 'http://localhost:5000/api',
 });
 
 export default API;
